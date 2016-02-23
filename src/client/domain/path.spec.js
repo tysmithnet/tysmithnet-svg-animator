ccf59293632178e1dfa797fcab0d0983fc72a500
Ty Smith <ty@tysmith.net>
2016-02-23 18:08:46 -0500

    Added first test for the path domain entity

---
+++ src/client/domain/path.spec.js
@@ -0,0 +1,9 @@
+import Path from './path';
+
+describe("path", () => {
+    it("should take a name and some data", () => {
+        let path = new Path("a name", "M 0 50z");
+        expect(path.name).toEqual("a name");
+        expect(path.data).toEqual("M 0 50z");
+    });
+})
