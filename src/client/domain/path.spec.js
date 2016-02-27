f6d2e08c4c051a0e4a9cfa18979c840a54e709d6
Ty Smith <ty@tysmith.net>
2016-02-27 13:39:13 -0500

    Removed path abstraction

--- src/client/domain/path.spec.js
+++
@@ -1,9 +0,0 @@
-import Path from "./path";
-
-describe("Path", () => {
-    it("should take a name and some data", () => {
-        let path = new Path("a name", "M 0 50z");
-        expect(path.name).toEqual("a name");
-        expect(path.data).toEqual("M 0 50z");
-    });
-})
