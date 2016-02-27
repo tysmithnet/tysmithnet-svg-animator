7c857f78653b8103c8d9d3a43bcc11f7a4fae353
Ty Smith <dts5004@gmail.com>
2016-02-27 17:38:43 -0500

    more organization to sequence spec

--- src/client/domain/sequencing/sequence.spec.js
+++ src/client/domain/sequencing/sequence.spec.js
@@ -1,8 +1,10 @@
 import Sequence from "./sequence";
 
 describe("Sequence", () => {
-    it("should require a name", () => {
-        let seq = new Sequence("sample");
-        expect(seq.name).toEqual("sample");
+    describe("creation", () => {
+        it("should require a name", () => {
+            let seq = new Sequence("sample");
+            expect(seq.name).toEqual("sample");
+        });
     });
 });
