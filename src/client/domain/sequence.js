30b5ee93da0a275eed8c6e09af8744c61c5b9cf5
Ty Smith <ty@tysmith.net>
2016-02-27 08:53:53 -0500

    Added sequence class

---
+++ src/client/domain/sequence.js
@@ -0,0 +1,15 @@
+/**
+ * Represents a composite structure of sub sequences and
+ * sequence items. Instances of this class capture the
+ * flow of the animation.
+ */
+export default class Sequence {
+    
+    /**
+     * Create a Sequence
+     * @param {string} name - the name of the sequence
+     */
+    constructor(name){
+        this.name = name;
+    }
+}
