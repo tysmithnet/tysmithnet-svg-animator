0551fccd36a5c6de6b56bc58a563c8fa9b80a4f2
Ty Smith <ty@tysmith.net>
2016-02-27 09:21:35 -0500

    Sequence is a SequenceItem

--- src/client/domain/sequence.js
+++ src/client/domain/sequence.js
@@ -1,15 +1,17 @@
+import SequenceItem from "./sequence-item";
+
 /**
  * Represents a composite structure of sub sequences and
  * sequence items. Instances of this class capture the
  * flow of the animation.
  */
-export default class Sequence {
+export default class Sequence extends SequenceItem {
     
     /**
      * Create a Sequence
      * @param {string} name - the name of the sequence
      */
     constructor(name){
-        this.name = name;
+        super(name);
     }
 }
