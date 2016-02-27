8578009e99499269a686cf625f1b8308816cf125
Ty Smith <ty@tysmith.net>
2016-02-27 13:43:49 -0500

    jsdoc for AddShapeSequenceItem

--- src/client/domain/sequencing/add-shape-sequence-item.js
+++ src/client/domain/sequencing/add-shape-sequence-item.js
@@ -1,7 +1,17 @@
 import Shape from "../shape";
 import SequenceItem from "./sequence-item";
 
+/**
+ * Represents a sequence item that will add a shape to the
+ * sequence for animating.
+ */
 export default class AddShapeSequenceItem extends SequenceItem {
+    
+    /**
+     * Creates a AddShapeSequenceItem
+     * @param {string} name - user friendly name for this item
+     * @param {Shape} shape - the shape to be added 
+     */
     constructor(name, shape) {
         super(name)
         this.shape = shape;
