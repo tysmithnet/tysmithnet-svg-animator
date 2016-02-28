5f72e2693a8844a16c3ef65b950379cdc9704c2b
Ty Smith <dts5004@gmail.com>
2016-02-27 21:24:57 -0500

    Reworked base app component files

--- src/client/app/app.controller.js
+++ src/client/app/app.controller.js
@@ -2,10 +2,6 @@ import angular from "angular";
 
 export default class AppController {
     constructor(){
-        this.greeting = "hello master";
+        
     }
 }
-
-// angular
-//     .module("app")
-//     .controller("AppController", AppController);
