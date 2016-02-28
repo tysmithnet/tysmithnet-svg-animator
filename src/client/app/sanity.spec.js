d269a17a16bb6b735bc0bef48ce0ba188d7d1a8f
Ty Smith <dts5004@gmail.com>
2016-02-27 20:34:32 -0500

    removed sanity spec

--- src/client/app/sanity.spec.js
+++
@@ -1,8 +0,0 @@
-import AppController from './app.controller';
-
-describe("sanity check", () => {
-     it("should pass", () => {
-         let controller = new AppController();
-         expect(controller.greeting).toEqual("hello master");
-     });
-});
