7b9129bbdc027baf87ee0bd4ded84278af71b5b0
tsmith <tsmith@zaginteractive.com>
2016-02-29 14:12:16 -0500

    Added test for navbar directive

--- src/client/app/navbar/navbar.spec.js
+++ src/client/app/navbar/navbar.spec.js
@@ -1,12 +1,17 @@
+import angular from "angular";
 import {module, inject} from "angular-mocks";
 
 describe("Navbar", () => {
     var $controller;
-
+    var $compile;
+    var $rootScope;
+    
     beforeEach(() => {
         module("navbar");
-        inject((_$controller_) => {
+        inject((_$controller_, _$compile_, _$rootScope_) => {
             $controller = _$controller_;
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
         });
     });
 
@@ -16,10 +21,24 @@ describe("Navbar", () => {
             expect(controller.appTitle).toEqual("hi");
         });
     });
-    
+
     describe("directive", () => {
+        var compiledElement;
+        
+        function getCompiledElement(){
+            let html = "<ts-navbar></ts-navbar>";
+            let scope = $rootScope.$new();
+            let compiled = $compile(html)(scope);
+            scope.$digest();
+            return compiled;
+        }
+        
+        beforeEach(() => {
+            compiledElement = getCompiledElement();
+        });
+        
         it("should display the app title", () => {
-            
+            expect(compiledElement).toBeDefined();
         });
     });
 });
