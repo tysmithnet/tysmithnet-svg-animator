afb21eb6850387005b747b0485ebfb521c3fc33d
Ty Smith <dts5004@gmail.com>
2016-03-01 19:04:51 -0500

    templateUrl directives are now testable

--- src/client/app/navbar/navbar.spec.js
+++ src/client/app/navbar/navbar.spec.js
@@ -5,16 +5,17 @@ describe("Navbar", () => {
     var $controller;
     var $compile;
     var $rootScope;
-    var $templateCache;
     
     beforeEach(() => {
         module("app");
         module("navbar");
-        inject((_$controller_, _$compile_, _$rootScope_, _$templateCache_) => {
+        module("ngMockE2E");
+        inject((_$controller_, _$compile_, _$rootScope_, _$templateCache_, _$httpBackend_) => {
             $controller = _$controller_;
             $compile = _$compile_;
             $rootScope = _$rootScope_;
-            var template = _$templateCache_.get("app/navbar/navbar.template.html");
+            let template = _$templateCache_.get("/app/navbar/navbar.template.html");
+            _$httpBackend_.when("GET", "/app/navbar/navbar.template.html").respond(template);
         });
     });
 
