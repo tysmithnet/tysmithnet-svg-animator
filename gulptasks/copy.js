75913c9017d74addaea03b59aa6535f21790ac4d
Ty Smith <dts5004@gmail.com>
2016-03-07 20:34:03 -0500

    remove test bootstrap file from copy task

--- gulptasks/copy.js
+++ gulptasks/copy.js
@@ -4,7 +4,7 @@ var newer = require("gulp-newer");
 gulp.task("copy", function(){
     var dest = "dist"
     return gulp
-        .src("src/**")
+        .src(["src/**", "!src/client/test/boot.js"])
         .pipe(newer(dest))
         .pipe(gulp.dest(dest));     
 });
