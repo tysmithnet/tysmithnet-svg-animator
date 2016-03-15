130fcd015ab40b54d32e78a202e9b702794544f9
Ty Smith <dts5004@gmail.com>
2016-03-14 21:39:30 -0400

    Add template cache gulp task
    ng-mocks httpbackend cannot retrieve templates, and so they need to be preinserted into $templateCache

---
+++ gulptasks/templatecache.js
@@ -0,0 +1,34 @@
+var gulp = require("gulp");
+var through = require("through2");
+var Vinyl = require("vinyl");
+var util = require("gulp-util");
+var debug = require("gulp-debug");
+var path = require("path");
+
+gulp.task("templatecache", function(cb){
+    var createTemplate = function(vinyl, encoding, callback){
+        var url = path.normalize("/" + vinyl.path.replace(vinyl.cwd, ""));
+        var contents = `
+        try {
+          angular.module("templates");
+        }
+        catch(){
+          angular.module("templates", []);
+        }
+        angular
+          .module("templates")
+          .run(function($templateCache){
+            $templateCache.put("${url}", \`${vinyl.contents.toString()}\`);
+          });
+        `;
+        vinyl.contents = new Buffer(contents);
+        vinyl.path = vinyl.path + ".js";
+        this.push(vinyl);
+        callback();
+    };
+
+    return gulp
+      .src("src/client/**/*.template.html", {base: "src"})
+      .pipe(through.obj(createTemplate))
+      .pipe(gulp.dest("dist/"))
+});
