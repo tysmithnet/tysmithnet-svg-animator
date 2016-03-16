4801790d0584375bf60012944277fdafdf5182d2
Ty Smith <dts5004@gmail.com>
2016-03-15 23:07:56 -0400

    Rearranged gulp build task sequence
    inject needs to come after copy because inject uses the dist folder as src

--- gulptasks/build.js
+++ gulptasks/build.js
@@ -2,5 +2,5 @@ var gulp = require("gulp");
 var runSequence = require("run-sequence");
 
 gulp.task("build", function(cb){
-    runSequence(["inject", "copy"], cb); 
+    runSequence(["copy", "templatecache"], "inject", cb); 
 });
