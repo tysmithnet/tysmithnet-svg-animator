850053f3aff2ee0480a876fb5b1d7aba39045fc3
Ty Smith <dts5004@gmail.com>
2016-03-15 23:09:05 -0400

    Refactored design of inject task
    The previous implementation was 1) incorrect and 2) needlessly complicated

--- gulptasks/inject.js
+++ gulptasks/inject.js
@@ -1,23 +1,45 @@
+"use strict";
+
 var gulp = require("gulp");
 var util = require("gulp-util");
 var filenames = require("gulp-filenames");
 var debug = require("gulp-debug");
 var path = require("path");
 var injectString = require("gulp-inject-string");
+var through = require("through2");
+var concatStream = require("concat-stream");
+var path = require("path");
 
-gulp.task("inject", function (cb) {
-    var testboot = "src/client/test/boot.js";
-    gulp
-        .src("src/client/**/*.spec.js")
-        .pipe(filenames("inject"))
-        .on("finish", function () {
-            var imports = filenames.get("inject", "relative").map(function(e){
-                return "import \"../" + e + "\";"; 
+const testboot = "src/client/test/boot.js";
+
+function getVinyls() {
+    return new Promise((res, rej) => {
+        gulp
+            .src("dist/client/**/*.spec.js")
+            .pipe(concatStream({
+                encoding: "object"
+            }, (vinyls) => {
+                res(vinyls);
+            }));
+    });
+}
+
+gulp.task("inject", (cb) => {
+    let vinylsPromise = getVinyls();
+
+    vinylsPromise.then((vinyls) => {
+        let text = vinyls.map((e) => {
+            return 'import "' +
+                path.normalize("../" + e.path.replace(e.cwd, "")) + '";';
+        }).join("\n");
+
+        gulp
+            .src(testboot)
+            .pipe(injectString.prepend(text))
+            .pipe(gulp.dest("dist/client/test"))
+            .on("finish", () => {
+                util.log("complete!");
+                cb();
             });
-            gulp
-                .src(testboot)
-                .pipe(injectString.prepend(imports.join("\n")))
-                .pipe(gulp.dest("dist/client/test/"))
-                .on("end", cb);
-        });
+    });
 });
