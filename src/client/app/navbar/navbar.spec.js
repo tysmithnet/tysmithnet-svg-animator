786f4f6b1b5c992ad397b340710a8bd1b90fe796
Ty Smith <dts5004@gmail.com>
2016-03-22 00:33:57 -0400

    Making progress on injecting controller
    $controllerProvider seems to give us the functionality we want, but using it breaks everything else

--- src/client/app/navbar/navbar.spec.js
+++ src/client/app/navbar/navbar.spec.js
@@ -6,18 +6,24 @@ import {templateUrl} from "./navbar.directive";
 
 describe("Navbar", () => {
     var $controller;
+    var $controllerProvider;
     var $compile;
     var $rootScope;
-
+    var $templateCache;
+    var $httpBackend;
+    
     beforeEach(() => {
         module("app");
         module("navbar");
         module("ngMockE2E");
 
-        inject((_$controller_, _$compile_, _$rootScope_, _$templateCache_, _$httpBackend_) => {
+        inject((_$controller_, _$compile_, _$rootScope_, _$templateCache_, _$httpBackend_, _$controllerProvider_) => {
             $controller = _$controller_;
             $compile = _$compile_;
             $rootScope = _$rootScope_;
+            $templateCache = _$templateCache_;
+            $httpBackend = _$httpBackend_;
+            $controllerProvider = _$controllerProvider_;
             let template = _$templateCache_.get(templateUrl);
             _$httpBackend_.when("GET", templateUrl).respond(template);
         });
@@ -36,13 +42,15 @@ describe("Navbar", () => {
         function getCompiledElement() {
             let html = "<ts-navbar></ts-navbar>";
             let scope = $rootScope.$new();
-            scope.navbar = $controller("NavbarController", { globals: { appTitle: "hi" } });
             let compiled = $compile(html)(scope);
             scope.$digest();
             return compiled;
         }
 
         beforeEach(() => {
+            $controllerProvider.register("NavbarController", function(){
+               this.appTitle = "hi"; 
+            });
             compiledElement = getCompiledElement();
         });
         
