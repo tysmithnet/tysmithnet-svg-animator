357580fdcb76f1d9e9803d9b2d659487359d77f0
Ty Smith <dts5004@gmail.com>
2016-05-03 21:20:13 -0400

    Added 2sec debounce to watch task

--- gulptasks/watch.js
+++ gulptasks/watch.js
@@ -6,5 +6,5 @@ var gulp = require("gulp");
  * Watch any build files for changes
  */
 gulp.task("watch", function(){
-    gulp.watch("src/**/*", ["build"]);
+    gulp.watch("src/**/*", {debounceDelay: 2000}, ["build"]);
 });
