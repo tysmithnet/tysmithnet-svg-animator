923d2a8ed60acdc574ee10cd8d00e3718e75d97b
Ty Smith <dts5004@gmail.com>
2016-05-03 22:00:37 -0400

    Added scene boot module to the master boot loader

--- src/client/app/bootstrap.js
+++ src/client/app/bootstrap.js
@@ -1,6 +1,8 @@
 import angular from "angular";
 import "bootstrap";
 
+// todo: these should be injected
 import "./app.boot";
 import "./navbar/navbar.boot";
 import "./viewer/viewer.boot";
+import "./scene/scene.boot";
