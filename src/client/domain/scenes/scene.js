a726aa92d9424700a4c626716f5a5996f7ec87fb
Ty Smith <dts5004@gmail.com>
2016-05-10 18:11:13 -0400

    Added scene display information to scene domain entity

--- src/client/domain/scenes/scene.js
+++ src/client/domain/scenes/scene.js
@@ -3,8 +3,9 @@
 import Sequence from "../sequencing/sequence";
 
 export default class Scene {
-    constructor(name) {
+    constructor(name, sceneDisplay) {
         this.name = name;
+        this.sceneDisplay = sceneDisplay;
         this.sequence = new Sequence("root");
     }
 }
