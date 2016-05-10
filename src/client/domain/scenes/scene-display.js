5f314ac4aaf0262c04257238f5446af89ddbe473
Ty Smith <dts5004@gmail.com>
2016-05-10 18:10:50 -0400

    Added additional domain entity classes for basic scene information This includes generic dimensions, viewbox, and scene-display

---
+++ src/client/domain/scenes/scene-display.js
@@ -0,0 +1,6 @@
+export default class SceneDisplay{
+    constructor(dimensions, viewbox){
+        this.dimensions = dimensions;
+        this.viewbox = viewbox;
+    }
+}
---
+++ src/client/domain/viewbox.js
@@ -0,0 +1,7 @@
+export default class ViewBox{
+    constructor(x, y, dimensions){
+        this.x = x;
+        this.y = y;
+        this.dimensions = dimensions;
+    }
+}
