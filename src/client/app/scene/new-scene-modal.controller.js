d925187788b0d3aff5ea78fef9d5d3d037636859
Ty Smith <dts5004@gmail.com>
2016-05-10 18:12:12 -0400

    Added additional logic to the new scene modal files This is approaching the necessary functionality required by the component

--- src/client/app/scene/new-scene-modal.controller.js
+++ src/client/app/scene/new-scene-modal.controller.js
@@ -1,19 +1,45 @@
 "use strict";
 
 import angular from "angular";
+import Scene from "domain/scenes/scene";
+import Dimensions from "domain/dimensions";
+import ViewBox from "domain/viewbox";
+import SceneDisplay from "domain/scenes/scene-display";
 
 /**
  * Backing controller for the new scene modal
  */
 export default class NewSceneModalController {
-    constructor($uibModalInstance){
+    constructor($uibModalInstance, $scope){
         this.$uibModalInstance = $uibModalInstance;
+        this.$scope = $scope;
+        this.name = null;
+        this.canvas = {
+            width: null,
+            height: null
+        };
+        this.viewbox = {
+            x: 0,
+            y: 0,
+            width: null,
+            height: null
+        };
+        
+        this.setupWatchers(); 
+    }
+    
+    setupWatchers(){
+        this.$scope.$watch("newSceneModalController.canvas", (oldValue, newValue) => {
+           
+        }, true);
     }
     
     /**
      * Called when OK is clicked on the modal
      */
     ok(){
+        let dimensions = new Dimensions(this.canvas.width, this.canvas.height);
+        let scene = new Scene();
         this.$uibModalInstance.close("hi");
     }
     
@@ -21,11 +47,11 @@ export default class NewSceneModalController {
      * Called when Cancel is clicked on the modal
      */
     cancel(){
-        ;
+        this.$uibModalInstance.dismiss("cancelled");
     }
 }
 
-NewSceneModalController.$inject = ["$uibModalInstance"];
+NewSceneModalController.$inject = ["$uibModalInstance", "$scope"];
 
 angular
     .module("scene")
