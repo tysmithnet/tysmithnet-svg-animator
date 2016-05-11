bf609ecd29b1bab14908b84a54654eeb5b618d7a
Ty Smith <dts5004@gmail.com>
2016-05-11 18:33:30 -0400

    Timeline should have been Sequencing from the get go

---
+++ src/client/app/sequencing/timeline.module.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import angular from "angular";
+
+/**
+ * The timeline module will contain components and services
+ * related to the creation and editing of the timelines. This
+ * is a major and integral piece of functionality required by
+ * the application.
+ */
+angular
+    .module("sequencing", []);
